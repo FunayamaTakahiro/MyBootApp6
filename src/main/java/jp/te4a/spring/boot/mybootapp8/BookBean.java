85d5435f7b2d79531933ce65083c48988b6b324f
200659 <200659@e0601-20.JC-21.local>
2023-06-01 12:57:14 +0900

    kadai8 commit

--- src/main/java/jp/te4a/spring/boot/mybootapp8/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/mybootapp8/BookBean.java
@@ -1,10 +1,12 @@
-package jp.te4a.spring.boot.mybootapp7;
+package jp.te4a.spring.boot.mybootapp8;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class BookBean {
 	Integer id;
 	String title;
